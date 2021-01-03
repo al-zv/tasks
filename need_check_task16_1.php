4f43bee6405ba4052e9bafd522a605ba7c630da4
al-zv <al3z1v@gmail.com>
2021-01-03 21:56:20 +0500

    comment task14

--- need_check_task16_1.php
+++ need_check_task16_1.php
@@ -7,19 +7,22 @@
 <body>
   <!-- используем форму ввода, для отображения введенного запроса с сайта https://www.php.net -->
   <form method="POST">
-    <input type="name" name="name_f">
+    <input type="name" name="name_f" value="">
     <input type="submit" value="submint">
     </form>
 
 <?php
 // подключаем файл с описанным классом Find
-require_once('task16_1_class.php');
+require_once('ok_task16_1_class.php');
 echo <<<'description'
 16.1. Создайте класс с методом find () , принимающий единственный аргумент­ строку с названием функции РНР. В качестве результата метод должен возвра­щать текст с сайта http://php.net с описанием функции.
 description;
 
     // В статический метод find класса Find передаем запрос с помощью глобального массива $_POST. Далее отображаем найденную страницу с сайте https://www.php.net
     echo "<br />";
+    if (empty($_POST['name_f'])) {
+      $_POST['name_f'] = "";
+    }
     echo Find::find($_POST['name_f']);
 
 ?>
